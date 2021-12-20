2035c733023e72fc0526c8f0d689df691dbc2f68
Jacob Stieneker <94584618+jacob-stieneker@users.noreply.github.com>
2021-12-20 00:10:29 -0600

    Add files via upload

---
+++ Landing_Page_Script.js
@@ -0,0 +1,11 @@
+function CallSpace() {
+window.location.replace("UAT_Space_HTML_Final_Rev.html");
+}
+
+function Robotics() {
+window.location.replace("https://www.uat.edu/media/UAT_Catalog_2021_V14.pdf#page=[37]");
+}
+
+function PlayCraps() {
+window.location.replace("Virtual_Casino_Sim.html");
+}
